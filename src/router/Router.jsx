65665dbdad18b8bc14c1e6cebdd4e311505888fd
sua <dssua0714@naver.com>
2025-03-05 13:51:04 +0900

    board

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import LottoPage from '@/ui/pages/LottoPage'
 import MainLayout from '../ui/layouts/MainLayout'
 import RspPage from '../ui/pages/RspPage'
+import BoardPage from '../ui/pages/BoardPage'
 
 
 const routes = [
@@ -20,6 +21,12 @@ const routes = [
                 path:'/rsp',
                 element:<RspPage />,
                 loader:() => '가위바위보',
+            },
+
+            {
+                path:'/boards',
+                element:<BoardPage />,
+                loader:() => '게시판',
             },
         ]
     },
